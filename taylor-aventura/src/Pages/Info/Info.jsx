7e859da459086b313f01084535860a79ca0c14ba
Mateo Lohezic <mateolohezic@gmail.com>
2023-06-17 17:31:57 -0300

    update

--- taylor-aventura/src/Pages/Info/Info.jsx
+++ taylor-aventura/src/Pages/Info/Info.jsx
@@ -7,8 +7,8 @@ function Info() {
     <>
       <div className='contenedorInformación'>
         <div>Mi nombre es Mateo, tengo 21 años. Hice este juego simplemente para divertir y entretener. Me ayudas mucho si compartes a tus amigas.</div>
-        <a href="https://www.instagram.com/mateo.lohezic/" target="_blank"><button>INSTAGRAM</button></a>
-        <div className='correoInfo'>Correo: mateolohezic@gmail.com</div>
+        {/* {/* <a href="https://www.instagram.com/mateo.lohezic/" target="_blank"><button>INSTAGRAM</button></a> */}
+        <div className='correoInfo'>Correo: tayloreligetupropiaaventura@gmail.com</div>
         <div className='botonEmpezar'><a href="/"><button type='button'>INICIO</button></a></div>
       </div>
       <div className='disclaimer'>No soy autor de ninguna imagen, y esto no tiene fines de lucro. Por cualquier problema contactarme.</div>
